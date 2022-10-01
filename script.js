87bad181076bd2c74c89bd99b62b5ac87b8ebb58
Vaibhavi Roy <vaibhaviroy2002@gmail.com>
2022-10-01 14:25:44 +0530

    Change color theme and background

--- script.js
+++ script.js
@@ -1,48 +1,40 @@
-const jokeEl = document.getElementById("joke"); // div id = joke
-const get_joke = document.getElementById("get_joke"); // button id = get_joke
+const jokeEl = document.getElementById('joke');          // div id = joke        
+const get_joke = document.getElementById('get_joke');    // button id = get_joke 
 const body = document.querySelector("body");
 const toggle = document.querySelector(".toggle");
-get_joke.addEventListener("click", generateJoke);
-generateJoke();
+let darkMode = false;
+get_joke.addEventListener('click', generateJoke);
+generateJoke();                                          
 
-async function generateJoke() {
-  // async makes a function return a Promise
-  // call the icanhaz API
-  var loading = document.getElementById("loading");
-  loading.style.visibility = "visible";
-  const jokeRes = await fetch("https://icanhazdadjoke.com/", {
-    // await makes a function wait for a Promise
-    headers: {
-      Accept: "application/json",
-    },
-  });
+async function generateJoke(){  // async makes a function return a Promise
+    // call the icanhaz API
+    const jokeRes = await fetch('https://icanhazdadjoke.com/', {      // await makes a function wait for a Promise
+        headers: {
+            'Accept': 'application/json'      
 
-  const joke = await jokeRes.json(); // save
-  loading.style.visibility = "hidden";
-//add-transition
 
         }
     });
 
     const joke = await jokeRes.json();  // save     
 
-      // Fade out
-      jokeEl.style.opacity = 0;
-  
-  // Fade in 
-  setTimeout(function(){ 
-     jokeEl.innerHTML = joke.joke; 
-      jokeEl.style.opacity = 1;
-  },500);
-     
-
-
-}
-  jokeEl.innerHTML = joke.joke;
+ 
+    jokeEl.innerHTML = joke.joke;    
 }
 toggle.addEventListener("click", () => {
-  body.classList.toggle("dark")
-    ? (toggle.firstElementChild.className = "far fa-moon")
-    : (toggle.firstElementChild.className = "far fa-sun");
-});
- //master
+    darkMode = darkMode ? false :true;
+
+    body.classList.toggle("dark")
+      ? (toggle.firstElementChild.className = "far fa-moon")
+      : (toggle.firstElementChild.className = "far fa-sun");
+      
+    if(darkMode){
+        document.querySelector('.bg').src="Background-dark2.jpg";
+        document.querySelector('body').classList.add('dark');
+    }
+    if(!darkMode){
+        document.querySelector('.bg').src="Background-light.jpg";
+        document.querySelector('body').classList.remove('dark');
+    }
+  });
+
